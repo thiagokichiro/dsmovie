f7178e8c68eb204d6639bc51429ce127c56d4619
Thiago Kichiro <kichiro.88@gmail.com>
2022-01-16 23:13:55 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,22 @@
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useState, useEffect } from "react";
+import { BASE_URL } from "utils/requests";
+import { MoviePage } from "types/movie"
+import axios from "axios";
 
 function Listing() {
+    const [pageNumber, setPageNumber] = useState(0);
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=1`)
+        .then(response => {
+            const data = response.data as MoviePage;
+            console.log(data);
+            setPageNumber(data.number);
+        });
+    }, []);
+    
     return (
         <>
             <Pagination />
